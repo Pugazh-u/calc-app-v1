ac6f2cd69e2e137d00aeec8ac048e535b50ded35
Pugazh-u <pugazh.u@oracle.com>
2026-07-29 14:28:21 +0530

    Initial git commit

---
+++ src/main/java/org/example/calcapp/service/Calculation.java
@@ -0,0 +1,20 @@
+package org.example.calcapp.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Calculation {
+    Logger logger= LoggerFactory.getLogger(Calculation.class);
+    public double add(double a,double b){
+        logger.debug("Adding {} and {}",a,b);
+        return a+b;
+    }
+    public double sub(double a,double b){
+        logger.debug("Sub {} and {}",a,b);
+        return a-b;
+    }
+    public double mul(double a,double b){
+        logger.debug("Mul {} and {}",a,b);
+        return a*b;
+    }
+}
---
+++ src/test/java/org/example/calcapp/service/CalculationTest.java
@@ -0,0 +1,42 @@
+package org.example.calcapp.service;
+
+import org.example.calcapp.service.Calculation;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CalculationTest {
+    Logger logger= LoggerFactory.getLogger(CalculationTest.class);
+    Calculation cal;
+
+    @BeforeEach
+    public void setUp() {
+        logger.info("set - up");
+        cal= new Calculation();
+    }
+
+    @Test
+    public void testAdd() {
+        Assertions.assertNotNull(cal, "Calculation is null");
+        Assertions.assertEquals(9,cal.add(4,5));
+    }
+    @Test
+    public void testSubtract() {
+        Assertions.assertNotNull(cal, "Calculation is null");
+        Assertions.assertEquals(9,cal.sub(19,10));
+    }
+    @Test
+    public void testMul() {
+        Assertions.assertNotNull(cal, "Calculation is null");
+        Assertions.assertEquals(9,cal.mul(9,1));
+    }
+    @AfterEach
+    public void tearDown() {
+        cal = null;
+        logger.info("tearDown:- intitaled and service instance destroyed");
+    }
+
+}
